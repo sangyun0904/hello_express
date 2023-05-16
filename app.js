826d8cfb6b5d9ac32da0a0d4539d404321812ee9
sangyoon kim <sangyun0904@gmail.com>
2023-05-16 22:16:05 +0900

    app.js listen 3000

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 const express = require("express")
 const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Home Page")
+})
+
+app.listen(3000, () => {
+    console.log('server is listening on port 3000...')
+})
+
+// app.get 
+// app.post 
+// app.put 
+// app.delete 
+// app.all 
+// app.user
+// app.listen
