99637d18de6dae9c15b63b19b97ad930c8462c17
sangyoon kim <sangyun0904@gmail.com>
2023-05-24 21:49:34 +0900

    mongo 연결 반복 추가

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ const app = express()
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
 
-mongoose
-    .connect(mongoURL)
-    .then(() => console.log("succesfully connected to DB"))
-    .catch((e) => console.log(e));
+const connectWithRetry = () => {
+    mongoose
+        .connect(mongoURL)
+        .then(() => console.log("succesfully connected to DB"))
+        .catch((e) => {
+            console.log(e)
+            setTimeout(connectWithRetry, 5000)
+    });
+}
+
+connectWithRetry();
 
 app.get("/", (req, res) => {
     res.send("<h2>Hi There!!!</h2>")
