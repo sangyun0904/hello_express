953fc489bb5b5b1e56b7e5f2fc17f04a3dbfb0eb
sangyoon kim <sangyun0904@gmail.com>
2023-05-25 09:59:39 +0900

    post model, controller, routes 추가

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 
+const postRouter = require("./routes/postRoutes")
+
 const app = express()
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
@@ -22,6 +24,7 @@ app.get("/", (req, res) => {
     res.send("<h2>Hi There!!!</h2>")
 });
 
+app.use("/posts", postRouter)
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening on port ${port}`))
