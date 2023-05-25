6d163928f3f123c7b89bb6e7466bead9e2f106f7
sangyoon kim <sangyun0904@gmail.com>
2023-05-25 10:59:52 +0900

    user login 추가

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,3 +23,36 @@ exports.signUp = async (req, res) => {
         })
     }
 }
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await User.findOne({username})
+
+        if (!user) {
+            res.status(404).json({
+                status: "fail",
+                message: "user not found",
+            })
+        }
+
+        const isCorrect = await bcrypt.compare(password, user.password)
+
+        if (isCorrect) {
+            res.status(200).json({
+                status: "success",
+            })
+        } else {
+            res.status(400).json({
+                status: 'fail',
+                message: "incorrect username or password",
+            })
+        }
+
+    } catch (e) {
+        res.status(400).json({
+            status: 'fail',
+            message: e,
+        })
+    }
+}
